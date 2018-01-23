6967c3956bafd774ff79475f49ecabc55e7c9820
zombitrafik <zombitrafik05@gmail.com>
2018-01-23 10:47:07 +0300

    add pugjs

---
+++ pugfile.js
@@ -0,0 +1,10 @@
+var pug = require('pug'),
+    fs = require('fs');
+
+var compileFn = pug.compileFile('./template/main.pug', {pretty: true});
+
+var html = compileFn(
+    require('./template-data.json')
+);
+
+fs.writeFileSync('./compiled.html', html, 'utf8');
